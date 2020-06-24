1d125b5135f6e59a1fa1e9db0edacdee11641f0f
Jef Spaleta <jspaleta@gmail.com>
2020-06-24 00:02:56 -0800

    Add pure ruby implementation of check-ssl-root-issuer.rb as alternative to check-ssl-anchor.rb

---
+++ bin/check-ssl-root-issuer.rb
@@ -0,0 +1,129 @@
+#! /usr/bin/env ruby
+#
+#   check-ssl-root-issuer
+#
+# DESCRIPTION:
+#   Check that a certificate is chained to a specific root certificate issuer
+#
+# OUTPUT:
+#   plain text
+#
+# PLATFORMS:
+#   Linux
+#
+# DEPENDENCIES:
+#   gem: sensu-plugin
+#
+# USAGE:
+#
+#   Check that a specific website is chained to a specific root certificate
+#       ./check-ssl-root-issuer.rb \
+#           -u https://example.com \
+#           -i "CN=DST Root CA X3,O=Digital Signature Trust Co."
+#
+# LICENSE:
+#   Copyright Jef Spaleta (jspaleta@gmail.com) 2020
+#   Released under the same terms as Sensu (the MIT license); see LICENSE
+#   for details.
+#
+
+require 'sensu-plugin/check/cli'
+require 'openssl'
+require 'uri'
+require "net/http"
+require "net/https"
+
+#
+# Check root certificate has specified issuer name
+#
+class CheckSSLRootIssuer < Sensu::Plugin::Check::CLI
+  option :url,
+         description: 'Url to check: Ex "https://google.com"',
+         short: '-u',
+         long: '--url URL',
+         required: true
+
+  option :issuer,
+         description: 'An X509 certificate issuer name, RFC2253 format Ex: "CN=DST Root CA X3,O=Digital Signature Trust Co."',
+         short: '-i',
+         long: '--issuer ISSUER_NAME',
+         required: true
+
+  option :regexp,
+         description: 'Treat the issuer name as a regexp',
+         short: '-r',
+         long: '--regexp',
+         default: false,
+         boolean: true,
+         required: false
+
+  option :format,
+         description: 'optional issuer name format. Defaults to RFC2253. Allowed values: RFC2253, ONELINE, COMPAT',
+         short: '-f',
+         long: '--format FORMAT_VAL',
+         default: 'RFC2253',
+         required: false
+
+
+  def validate_opts
+    config[:issuer_format] = OpenSSL::X509::Name::RFC2253
+    config[:issuer_format] = OpenSSL::X509::Name::ONELINE if config[:format] == 'ONELINE' 
+    config[:issuer_format] = OpenSSL::X509::Name::COMPAT  if config[:format] == 'COMPAT' 
+  end
+
+  def validate_issuer(cert)
+    issuer = cert.issuer.to_s(config[:issuer_format])
+    if config[:regexp]
+      ra = Regexp.new(config[:issuer].to_s)
+      return  issuer =~ ra
+    else
+      return issuer == config[:issuer].to_s
+    end
+  end
+
+  def find_root_cert(uri)
+    pcert = root_cert = nil
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.open_timeout = 10
+    http.read_timeout = 10
+    http.use_ssl = true
+    http.cert_store = OpenSSL::X509::Store.new
+    http.cert_store.set_default_paths
+    http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+
+    http.verify_callback = -> (verify_ok, store_context) {
+      root_cert=store_context.current_cert unless root_cert
+      pcert=store_context.current_cert
+      if ! verify_ok
+        failed_cert = store_context.current_cert
+        failed_cert_reason = [store_context.error, store_context.error_string] if store_context.error != 0
+      end
+      verify_ok
+    }
+    http.start { }
+    return root_cert
+  end
+
+  # Do the actual work and massage some data
+
+  def run
+    failed_cert = nil
+    failed_cert_reason = "Unknown"
+    validate_opts
+    uri = URI.parse(config[:url])
+    critical "Url must be https"  if uri.scheme != 'https'
+    root_cert=find_root_cert(uri)
+    if failed_cert
+      msg = "Certificate verification failed.\n Reason: #{failed_cert_reason}"
+      critical msg
+    end
+
+    if validate_issuer(root_cert)
+      msg = "Root certificate in chain has expected issuer name"
+      ok msg
+    else
+      msg = "Root certificate issuer did not match expected name.\nFound: \"#{root_cert.issuer.to_s(config[:issuer_format])}\"" 
+      critical msg
+    end
+  end
+end
