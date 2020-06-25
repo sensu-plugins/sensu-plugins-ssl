e5f3dc1928bd693685f1270308c085e13fc112ef
Jef Spaleta <jspaleta@gmail.com>
2020-06-24 23:01:31 -0800

    remove files changed in PR #70, unrelated to this new feature

--- bin/check-ssl-anchor.rb
+++ bin/check-ssl-anchor.rb
@@ -57,14 +57,6 @@ class CheckSSLAnchor < Sensu::Plugin::Check::CLI
          long: '--anchor ANCHOR_VAL',
          required: true
 
-  option :regexp,
-         description: 'Treat the anchor as a regexp',
-         short: '-r',
-         long: '--regexp',
-         default: false,
-         boolean: true,
-         required: false
-
   option :servername,
          description: 'Set the TLS SNI (Server Name Indication) extension',
          short: '-s',
@@ -87,7 +79,7 @@ class CheckSSLAnchor < Sensu::Plugin::Check::CLI
                 -servername #{config[:servername]} < /dev/null 2>&1`.match(/Certificate chain(.*)---\nServer certificate/m)[1].split(/$/).map(&:strip)
     data = data.reject(&:empty?)
 
-    unless data[0] =~ /0 s:\/?CN ?=.*/m
+    unless data[0] =~ /0 s:\/CN=.*/m
       data = 'NOTOK'
     end
     data
@@ -99,22 +91,11 @@ class CheckSSLAnchor < Sensu::Plugin::Check::CLI
     if data == 'NOTOK'
       critical 'An error was encountered while trying to retrieve the certificate chain.'
     end
-    puts config[:regexp]
-    # rubocop:disable Style/IfInsideElse
-    if config[:regexp]
-      ra = Regexp.new(config[:anchor].to_s)
-      if data[-1] =~ ra
-        ok 'Root anchor has been found.'
-      else
-        critical 'Root anchor did not match regexp /' + config[:anchor].to_s + "/\nFound \"" + data[-1] + '" instead.'
-      end
+
+    if data[-1] == config[:anchor].to_s
+      ok 'Root anchor has been found.'
     else
-      if data[-1] == config[:anchor].to_s
-        ok 'Root anchor has been found.'
-      else
-        critical 'Root anchor did not match string "' + config[:anchor].to_s + "\"\nFound \"" + data[-1] + '" instead.'
-      end
+      critical 'Root anchor did not match. Found "' + data[-1] + '" instead.'
     end
-    # rubocop:enable Style/IfInsideElse
   end
 end
